c97641d73e5b2fccfadbfa83e1ec14177071264f
Romainrev <32880241+Romainrev@users.noreply.github.com>
2018-02-09 14:48:33 +0100

    Ajout d'un formulaire de conversion de devise + fonction incomplète

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+include_once ('./function/convert.php');
+?>
+
+<!DOCTYPE>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>On part en Voyage ! </title>
+<body>
+<form method="post" action="">
+
+    <label for = "euro" name = "Euro">Somme en euro : </label><input type = "int" name = "chiffre" >
+    <input type = "submit" value = "Convertir" name ="Convertir">
+    <label for = "dollar" name = "Dollar">Resultat en dollar : </label><input type = "float" name = "chiffres" >
+
+
+</form>
+
+</body>
+</head>
+</html>
+
+
+
